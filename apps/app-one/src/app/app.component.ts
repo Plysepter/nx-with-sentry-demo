c800a5e3b26e9b5a8f9eb9f4ca21a03b507f1d14
Plysepter <brett@skywatch.com>
2021-08-18 01:03:56 -0400

    feat(app-one): suspect commit
    uploading intentionally problematic change to verify Sentry is configured correctly

--- apps/app-one/src/app/app.component.ts
+++ apps/app-one/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'nx-with-sentry-demo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-one';
+
+  ngOnInit() {
+    throw new Error('Very suspicious error');
+  }
 }
